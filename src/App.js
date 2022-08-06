f920d29b5f076289799cec8da7aa7d289c25c4a6
Nina NJul <nina.dev.web@gmail.com>
2022-08-06 15:08:15 +0300

    feat: add search

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('fairy');
+  const [animeData, setAnimeData] = useState();
+
   const getData = async () => {
-    const res = await fetch(`https://api.jikan.moe/v4/anime?q=fairy&limit=20`);
+    const res = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${search}&limit=10`
+    );
     const resData = await res.json();
-    console.log(resData);
+    // console.log(resData.data);
+    setAnimeData(resData.data);
   };
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [search]);
 
-  return <></>;
+  return (
+    <>
+      <div className='header'>
+        <h1>My Anime List</h1>
+        <div className='search-box'>
+          <input
+            type='search'
+            placeholder='Search your anime'
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
