c909c348deb8b0968c782a9377f686b9c4101074
Nina NJul <nina.dev.web@gmail.com>
2022-08-06 11:33:48 +0300

    init: start anime app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const getData = async () => {
+    const res = await fetch(`https://api.jikan.moe/v4/anime?q=fairy&limit=20`);
+    const resData = await res.json();
+    console.log(resData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <></>;
 }
 
 export default App;
