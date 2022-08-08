44a9b9ab034a3afa2fcc0de1bfd4ada6dc2a7bf8
Nina NJul <nina.dev.web@gmail.com>
2022-08-08 08:02:28 +0300

    feat: add overlay with anime description

--- src/components/AnimeList.js
+++ src/components/AnimeList.js
@@ -15,6 +15,14 @@ export const AnimeList = ({ animelist, setAnimeInfo }) => {
                 <img src={anime.images.jpg.large_image_url} alt='animeImage' />
                 <div className='anime-info'>
                   <h4>{anime.title}</h4>
+                  <div className='overlay'>
+                    <h4 className='title-overlay'>{anime.title_japanese}</h4>
+                    <br />
+                    <h3 className='title-overlay'>SYNOPSIS</h3>
+                    <div className='synopsis'>
+                      <p>{anime.synopsis}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             );
